694ddf777b9c23f5f95069a44b5f0e89ef6f448c
Christian Voegele <voegele@fortiss.org>
2014-08-04 13:10:22 +0000

    changed random number generator

--- src/net/sf/markov4jmeter/sessiongenerator/Configuration.java
+++ src/net/sf/markov4jmeter/sessiongenerator/Configuration.java
@@ -5,10 +5,10 @@ public class Configuration {
 	public static final String directory = "C:/Users/voegele/Applications/eclipse-jee-kepler-SR2-win32-x86_64/eclipse/workspace/net.sf.markov4jmeter.sessionGenerator/output/";
 	public static final int nrOfSessions = 10000;
 	
-	// min offSet in nanoseconds.
-    public static final long timeOffSetMin = 1000000000;
+	// min offSet in milliseconds.
+    public static final int timeOffSetMin = 1000;
     
-	// max offSet in nanoseconds.
-    public static final long timeOffSetMax = 2000000000;
+	// max offSet in milliseconds.
+    public static final int timeOffSetMax = 2000;
 		
 }
--- src/net/sf/markov4jmeter/sessiongenerator/SessionGenerator.java
+++ src/net/sf/markov4jmeter/sessiongenerator/SessionGenerator.java
@@ -1,6 +1,7 @@
 package net.sf.markov4jmeter.sessiongenerator;
 
 import java.io.IOException;
+import java.util.Random;
 import java.util.UUID;
 import java.util.concurrent.TimeUnit;
 
@@ -335,12 +336,26 @@ public class SessionGenerator {
 
 
 	/**
-	 * @param min
-	 * @param max
-	 * @return
+	 * Returns a pseudo-random number between min and max, inclusive.
+	 * The difference between min and max can be at most
+	 * <code>Integer.MAX_VALUE - 1</code>.
+	 *
+	 * @param min Minimum value
+	 * @param max Maximum value.  Must be greater than min.
+	 * @return Integer between min and max, inclusive.
+	 * @see java.util.Random#nextInt(int)
 	 */
-	public long randLong(long min, long max) {
-	    return (new java.util.Random().nextLong() % (max - min)) + min;
+	public static long randLong(int min, int max) {
+
+	    // NOTE: Usually this should be a field rather than a method
+	    // variable so that it is not re-seeded every call.
+	    Random rand = new Random();
+
+	    // nextInt is normally exclusive of the top value,
+	    // so add 1 to make it inclusive
+	    int randomNum = rand.nextInt((max - min) + 1) + min;
+
+	    return TimeUnit.NANOSECONDS.convert(randomNum, TimeUnit.MILLISECONDS);
 	}
 	
 }
