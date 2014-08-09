340f633e870f68d6b179e95732175ae1792406d0
Christian Voegele <voegele@fortiss.org>
2014-08-09 12:08:04 +0000

    new version

--- src/net/sf/markov4jmeter/sessiongenerator/Configuration.java
+++ src/net/sf/markov4jmeter/sessiongenerator/Configuration.java
@@ -2,7 +2,7 @@ package net.sf.markov4jmeter.sessiongenerator;
 
 public class Configuration {
 
-	public static final String directory = "C:/Users/voegele/Applications/eclipse-jee-kepler-SR2-win32-x86_64/eclipse/workspace/net.sf.markov4jmeter.sessionGenerator/output/";
+	public static final String directory = "workspace/net.sf.markov4jmeter.sessionGenerator/output";
 	public static final int nrOfSessions = 20000;
 	
 	// min offSet in milliseconds.
